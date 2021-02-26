a85c49c73ad95e235d14a187fde92c8736c08f87
Saadia <saadia.bayou@gmail.com>
2021-02-26 11:32:11 +0100

    modif nom fichier

--- fct_2var.py
+++ fct_2var.py
@@ -11,10 +11,11 @@ Created on Fri Feb 26 08:25:10 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Fonction cercle
-def cercle(x,y):
-    r=x**2+y**2
-    return r
+# Fonction à 2 variables
+def fxy (x,y):
+    #cercle
+    return x**2+y**2
+
 
 
 # Dimensions repère
@@ -25,30 +26,29 @@ y=np.arange(-10,10)
 X,Y=np.meshgrid(x,y)
 
 # Équation du cercle - fonction à deux variables
-R=cercle(X,Y)
+z=fxy(X,Y)
 
 # Tracé cercles
-plt.contourf(x,y,R)
+plt.contourf(X,Y,z)
 # Titres et légendes
-plt.title("Rayons-cercles")
+plt.title("Graphe fxy")
 
 plt.xlabel("Axe des absicces")
 plt.ylabel("Axe des ordonnées")
 # Sauvegarde
-plt.savefig("fig1-Rayons.png")
+plt.savefig("Fig1-Graphe.png")
 # Visualisation
 plt.colorbar()
 plt.show()
 
 
 # Tracé contours cercles
-plt.contour(X,Y,R)
-
+plt.contour(X,Y,z)
 # Titre et légendes
-plt.title("Contours rayons - cercles")
+plt.title("Contours fxy")
 plt.xlabel("Axe des abscisses")
 plt.ylabel("Axe des ordonnées")
 # Sauvegarde
-plt.savefig("fig2-Contours-cercle.png")
+plt.savefig("Fig2-Contours.png")
 # Visualisation
 plt.show()
